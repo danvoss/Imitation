54c5a3a087d6f97a53f39cdf6b20a3123505a1d2
Dan <danielandrewvoss@gmail.com>
2016-10-20 13:52:38 -0400

    Add StringReverse

---
+++ src/com/dvoss/StringReverse.java
@@ -0,0 +1,18 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 10/20/16.
+ */
+
+// www.tutorialspoint.com/javaexamples
+
+public class StringReverse {
+
+    public static void main(String[] args) {
+
+        String s = "ABCDEF";
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(s);
+        System.out.println(rev);
+    }
+}
