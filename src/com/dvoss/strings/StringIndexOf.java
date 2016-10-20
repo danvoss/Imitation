ab018a50f80ff5e47902a80530f23358f1e6bc49
Dan <danielandrewvoss@gmail.com>
2016-10-20 13:55:56 -0400

    Add StringIndexOf

---
+++ src/com/dvoss/strings/StringIndexOf.java
@@ -0,0 +1,17 @@
+package com.dvoss.strings;
+
+/**
+ * Created by Dan on 10/20/16.
+ */
+
+// www.tutorialspoint.com/javaexamples
+
+public class StringIndexOf {
+
+    public static void main(String[] args) {
+
+        String s = "Hello, world!";
+        int i = s.indexOf("world");
+        System.out.println("Found at index " + i);
+    }
+}
