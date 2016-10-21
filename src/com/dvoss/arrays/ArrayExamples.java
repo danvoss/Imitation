222abddaffd454caebd98f0a8785c6c5f447cb32
Dan <danielandrewvoss@gmail.com>
2016-10-21 15:21:30 -0400

    Add array example

--- src/com/dvoss/arrays/ArrayExamples.java
+++ src/com/dvoss/arrays/ArrayExamples.java
@@ -15,12 +15,23 @@ public class ArrayExamples {
         // sort and binary search
         int array1[] = {2, 5, -2, 6, -3, 8, 0, -7, -9, 4};
         Arrays.sort(array1);
+            // print with loop:
         for (int i = 0; i < array1.length; i++) {
             System.out.print(array1[i] + " ");
         }
         System.out.println(" ");
+            // print with toString:
+        System.out.println(Arrays.toString(array1));
+
         int indexA1 = Arrays.binarySearch(array1, 2);
         System.out.println(indexA1);
+
+        // find size of multi-dimensional array
+        String[][] data = new String[5][10];
+        System.out.println(data.length); // 5
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i].length); // always 10
+        }
 
     }
 }
