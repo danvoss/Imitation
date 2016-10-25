4133d029e6f2f3ee4adc17d8a8a14a89415c5bdf
Dan <danielandrewvoss@gmail.com>
2016-10-25 12:24:37 -0400

    Add method example - instance of

---
+++ src/com/dvoss/methods/Main.java
@@ -0,0 +1,35 @@
+package com.dvoss.methods;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ * Created by Dan on 10/25/16.
+ */
+
+// www.tutorialspoint.com/javaexamples
+
+public class Main {
+
+    public static void displayObjectClass(Object o) {
+        if (o instanceof Vector) {
+            System.out.println("Object is an instance of java.util.Vector");
+        }
+        else if (o instanceof ArrayList) {
+            System.out.println("Object is an instance of java.util.ArrayList");
+        }
+        else {
+            System.out.println("Object is an instance of " + o.getClass());
+        }
+    }
+
+    public static void main(String[] args) {
+        Object test1 = new ArrayList<>();
+        Object test2 = new Vector<>();
+        Object test3 = new HashMap<>();
+        displayObjectClass(test1);
+        displayObjectClass(test2);
+        displayObjectClass(test3);
+    }
+}
